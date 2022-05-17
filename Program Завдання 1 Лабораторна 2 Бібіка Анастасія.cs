a3fc642fa62824a058f472cbc2d24e3a6a2f45d5
Anastasiia-Bibika <93943067+Anastasiia-Bibika@users.noreply.github.com>
2022-05-17 20:31:52 +0300

    Laboratory 2

---
+++ Program Завдання 1 Лабораторна 2 Бібіка Анастасія.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labka1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть n: ");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+            Random rnd = new Random();
+            int[] b = new int[n];
+            int max = 0;
+            int kol = 0;
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = rnd.Next(1, 10);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(a[i] + "\t");
+            }
+            for (int j = 0; j < n; j++)
+            {
+                b[j] = (n-kol)*a[j];
+                kol++;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(b[i] + "\t");
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (b[i] > max)
+                {
+                    max = b[i];
+                }
+            }
+            Console.WriteLine($"max = {max}");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Program Завдання 2 Лабораторна 2 Бібіка Анастасія.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labaratory2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть розмірність матриці: ");
+            int n = int.Parse(Console.ReadLine());
+            int[,] a = new int[n, n];
+            Random rnd = new Random();
+           for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    a[i, j] = rnd.Next(1, 10);
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(a[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+            int min = a[0, 0];
+            int minvalue = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                   if (a[i,j]< min)
+                    {
+                        min = a[i, j];
+                        minvalue = j;
+                    }
+                }
+            }
+         Console.Write($" Найменший елемент знаходиться у {minvalue} стовпці");
+         Console.ReadLine();
+        }
+
+    }
+}
