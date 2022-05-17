107b0da05f4d355681873dcffa10a2285005118e
Anastasiia-Bibika <93943067+Anastasiia-Bibika@users.noreply.github.com>
2022-05-17 20:30:58 +0300

    Laboratory 1

---
+++ Завдання 2 Лабораторна робота 1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Zavd2());
+        }
+       
+
+        static string Zavd2()
+        {
+            Console.WriteLine("Zavd 2");
+            Console.Write("number = ");
+            int number = int.Parse(Console.ReadLine());
+            int[] numbers = new int[4];
+            int min = number % 10;
+            int minPosition = 3;
+
+            for (int i = 3; i >= 0; i--)
+            {
+                int x = number % 10;
+                if (x < min)
+                {
+                    min = x;
+                    minPosition = i;
+                }
+                if (number >= 1 && number <= 10)
+                {
+                    x = number;
+                }
+
+                numbers[i] = x;
+                number /= 10;
+            }
+            numbers[minPosition] = numbers[2];
+            numbers[2] = min;
+            return String.Join("", numbers);
+        }
+    }
+}
---
+++ Завдання перше лабораторна робота 1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("b=");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("c=");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double max1 = Max1int(a,b,c);
+            Console.WriteLine(max1);
+            double min = Minint(a,b,c);
+            Console.WriteLine(min);
+            double max2 = Max2int(a, b, c);
+            Console.WriteLine(max2);
+            double y = max1 + min + max2;
+            Console.WriteLine("Y = {0}", y);
+        }
+        static double Max1int(double a, double b, double c)
+        {
+            if ((a * b)> (b * c))
+            {
+                return (a * b);
+            }
+            else
+            {
+                return (b * c);
+            }
+        }
+        static double Minint(double a, double b, double c)
+        {
+            if ((a + c)< (a + b))
+            {
+                return (a + c);
+            }
+            else
+            {
+                return (a + b);
+            }
+        }
+        static double Max2int(double a, double b, double c)
+        {
+            if ((Math.Pow(a, 2) - Math.Pow(b, 2)) > (Math.Pow(b, 2) + Math.Pow(c, 2)))
+            {
+                return (Math.Pow(a, 2) - Math.Pow(b, 2));
+            }
+           else
+            {
+                return (Math.Pow(b, 2) + Math.Pow(c, 2));
+            }
+        }
+
+    }
+}
